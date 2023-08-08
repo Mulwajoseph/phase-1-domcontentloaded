0dd4d5f7d0e915e46fa51e2b88fba07ae0963da9
mulwajoseph <mulwajose.jm@gmail.com>
2023-08-08 07:09:25 +0300

    committed

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded' , () => {
+    console.log('This is really cool!')
+})
+console.log('This is really cool!')
+console.log(document.querySelector('#text'))
+// document.addEventListener('DOMContentLoaded', () => {
+//     const textElement = document.querySelector('#text');
+//     textElement.textContent = 'This is really cool!';
+//   });
+  
